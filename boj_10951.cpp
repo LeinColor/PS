5344f29c438d7520cf10233cb5f4d7d8986fd3c2
LeinColor <leincolor@gmail.com>
2021-07-07 20:17:41 +0900

    boj

---
+++ boj_10951.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	while (true) {
+		int A, B;
+		cin >> A >> B;
+		if (cin.eof())
+			break;
+
+		cout << A + B << '\n';
+	}
+
+	return 0;
+}
