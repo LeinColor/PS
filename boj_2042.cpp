f368444e73a38df7b847ba8c0f298c4ba8a7dec4
Lein <leincolor@gmail.com>
2022-01-06 23:55:06 +0900

    boj

--- boj_2042.cpp
+++ boj_2042.cpp
@@ -1,21 +1,78 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
-const int MAX_N = 10000000;
-int arr[MAX_N];
-int sum(int L, int R, int nodeNum, int nodeL, int nodeR) {
-	if (R < nodeL || nodeR < L) return 0;
-	if (L <= nodeL && nodeR <= R) return arr[nodeNum];
-	int mid = (nodeL + nodeR) / 2;
-	return sum(L, R, nodeNum * 2, nodeL, mid) + sum(L, R, nodeNum * 2 + 1, mid + 1, nodeR);
+using ll = long long;
+
+int N, M, K;
+vector<ll> segment_tree;
+vector<ll> arr;
+ll make_segment_tree(int node, int start, int end)
+{
+	if (start == end) return segment_tree[node] = arr[start];
+
+	int mid = (start + end) / 2;
+	ll left_result = make_segment_tree(node * 2, start, mid);
+	ll right_result = make_segment_tree(node * 2 + 1, mid + 1, end);
+	segment_tree[node] = left_result + right_result;
+
+	return segment_tree[node];
 }
 
-void update(int i, int val) {
-	
+ll sum(int node, int start, int end, int left, int right)
+{
+	if (left > end || right < start) return 0;
+	if (left <= start && end <= right) return segment_tree[node];
+
+	int mid = (start + end) / 2;
+	ll left_result = sum(node * 2, start, mid, left, right);
+	ll right_result = sum(node * 2 + 1, mid + 1, end, left, right);
+	return left_result + right_result;
 }
 
+void update_segment_tree(int node, int start, int end, int index, ll diff)
+{
+	if (index < start || index > end) return;
+	segment_tree[node] = segment_tree[node] + diff;
+
+	if (start != end)
+	{
+		int mid = (start + end) / 2;
+		update_segment_tree(node * 2, start, mid, index, diff);
+		update_segment_tree(node * 2 + 1, mid + 1, end, index, diff);
+	}
+}
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
+	cin >> N >> M >> K;
+	for (int i = 0; i < N; i++) {
+		int a; cin >> a;
+		arr.push_back(a);
+	}
+
+	int tree_height = (int)ceil(log2(N));
+	int tree_size = (1 << (tree_height + 1));
+	segment_tree.resize(tree_size);
+	make_segment_tree(1, 0, N - 1);
+
+	for (int i = 0; i < M + K; i++) {
+		int a, b;
+		ll c;
+		cin >> a >> b >> c;
+		if (a == 1) {
+			ll diff = c - arr[b - 1];
+			arr[b - 1] = c;
+			update_segment_tree(1, 0, N - 1, b - 1, diff);
+		}
+		else if (a == 2) {
+			cout << sum(1, 0, N - 1, b - 1, c - 1) << '\n';
+		}
+	}
 	return 0;
 }
